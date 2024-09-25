63f5f70545fe0ba95ff4098133ee6788650a3499
emilienbld <98468414+emilienbld@users.noreply.github.com>
2024-09-25 10:49:45 +0200

    Ajout photo de profil

---
+++ assets/photo/pp.php
@@ -0,0 +1,7 @@
+<h2>Changer la photo de profil</h2>
+<div class="pp-user">
+    <form action="../../src/service/upload_pic.php" method="POST" enctype="multipart/form-data" class="d-flex flex-row flex-responsive">
+        <input type="file" class="edit-pp-button" name="profil_pic" id="profil_pic" accept="image/*" required>
+        <button type="submit" class="save-pp-button">Téléverser la photo</button>
+    </form>
+</div>
